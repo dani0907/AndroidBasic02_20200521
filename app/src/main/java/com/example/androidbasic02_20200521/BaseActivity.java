8161115980ca6a45cfc8f13916022f34910c4cec
dy696786 <dy696786@gmail.com>
2020-05-21 12:36:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/androidbasic02_20200521/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.example.androidbasic02_20200521;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+//    abstract넣으면 내용물 당장 없어도 ㄱㅊ
+
+}
