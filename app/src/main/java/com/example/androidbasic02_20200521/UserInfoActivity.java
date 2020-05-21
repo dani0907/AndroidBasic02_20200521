103d8d6f4ba8aa7240dd0aae6fb28bc4dfec82de
dy696786 <dy696786@gmail.com>
2020-05-21 13:14:13 +0900

    사용자 이름을 일단 물음표로 표기

--- app/src/main/java/com/example/androidbasic02_20200521/UserInfoActivity.java
+++ app/src/main/java/com/example/androidbasic02_20200521/UserInfoActivity.java
@@ -1,15 +1,19 @@
 package com.example.androidbasic02_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.androidbasic02_20200521.databinding.ActivityUserInfoBinding;
+
 public class UserInfoActivity extends BaseActivity {
 
+    ActivityUserInfoBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_info);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_user_info);
         setupEvents();
         setValues();
     }
@@ -21,6 +25,6 @@ public class UserInfoActivity extends BaseActivity {
 
     @Override
     public void setValues() {
-
+        binding.userNameTxt.setText("???");
     }
 }
